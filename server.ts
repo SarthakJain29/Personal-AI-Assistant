9584683c59d2d3f8730d82818c0f8968677b5cae
Sarthak <sarthakjain3241@gmail.com>
2025-12-10 13:18:24 +0530

    express endpoint and google oauth integration

---
+++ server.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import type { Express, Request, Response } from "express";
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = 3000;
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URL
+);
+
+app.get("/auth", (req: Request, res: Response) => {
+  //generate the link for Oauth
+  const scopes = ["https://www.googleapis.com/auth/calendar"];
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: scopes,
+  });
+
+  res.redirect(url);
+});
+
+app.get("/callback", async (req: Request, res: Response) => {
+  //after user accepts on consent screen, google will hit this endpoint with a code
+  const code = req.query.code as string;
+
+  //now sending code + client id + secret to receive access & refresh tokens
+  const { tokens } = await oauth2Client.getToken(code);
+  console.log("Tokens", tokens);
+
+  console.log("Connected ✅");
+});
+
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
